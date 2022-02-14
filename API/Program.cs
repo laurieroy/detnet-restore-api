4b8c32ace43c6f463eb5d5cb58dfa48cebd500ef
Laurie Roy <laurieroy.dev@gmail.com>
2022-02-13 23:26:00 -0700

    Add dbcontext, store, product and seed data

--- API/Program.cs
+++ API/Program.cs
@@ -1,9 +1,8 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using API.Data;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
@@ -11,16 +10,30 @@ namespace API
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static void Main (string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder (args).Build ();
+            using var scope = host.Services.CreateScope ();
+            var context = scope.ServiceProvider.GetRequiredService<StoreContext> ();
+            var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>> ();
+            try
+            {
+                context.Database.Migrate ();
+                DbInitializer.Initialize (context);
+            }
+            catch (Exception ex)
+            {
+                logger.LogError (ex, "Problem migrating data");
+            }
+
+            host.Run ();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+        public static IHostBuilder CreateHostBuilder (string[] args) =>
+            Host.CreateDefaultBuilder (args)
+            .ConfigureWebHostDefaults (webBuilder =>
+            {
+                webBuilder.UseStartup<Startup> ();
+            });
     }
 }
