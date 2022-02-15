405f591ec0319b1524495cba92cf034d29ce46fc
lauretteroy <laurieroy.dev@gmail.com>
2022-02-14 20:37:39 -0800

    Restore sqlite changes from vscode mixup

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,4 +1,3 @@
-using System;
 using API.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -28,41 +27,11 @@ namespace API
             {
                 c.SwaggerDoc ("v1", new OpenApiInfo { Title = "WebAPIv5", Version = "v1" });
             });
-            services.AddDbContext<StoreContext> (options =>
+            services.AddDbContext<StoreContext> (opt =>
             {
-                var env = Environment.GetEnvironmentVariable ("ASPNETCORE_ENVIRONMENT");
-
-                string connStr;
-
-                if (env == "Development")
-                {
-                    // Use connection string from file.
-                    connStr = Configuration.GetConnectionString ("DefaultConnection");
-                }
-                else
-                {
-                    // Use connection string provided at runtime by Heroku.
-                    var connUrl = Environment.GetEnvironmentVariable ("DATABASE_URL");
-
-                    // Parse connection URL to connection string for Npgsql
-                    connUrl = connUrl.Replace ("postgres://", string.Empty);
-                    var pgUserPass = connUrl.Split ("@") [0];
-                    var pgHostPortDb = connUrl.Split ("@") [1];
-                    var pgHostPort = pgHostPortDb.Split ("/") [0];
-                    var pgDb = pgHostPortDb.Split ("/") [1];
-                    var pgUser = pgUserPass.Split (":") [0];
-                    var pgPass = pgUserPass.Split (":") [1];
-                    var pgHost = pgHostPort.Split (":") [0];
-                    var pgPort = pgHostPort.Split (":") [1];
-
-                    connStr = $"Server={pgHost};Port={pgPort};User Id={pgUser};Password={pgPass};Database={pgDb};SSL Mode=Require;Trust Server Certificate=true";
-                }
-
-                // Whether the connection string came from the local development configuration file
-                // or from the environment variable from Heroku, use it to set up your DbContext.
-                options.UseNpgsql (connStr);
+                opt.UseSqlite (Configuration.GetConnectionString ("DefaultConnection"));
             });
-            services.AddCors ();
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -79,9 +48,9 @@ namespace API
 
             app.UseRouting ();
 
-            app.UseCors (opt =>
+            app.UseCors(opt => 
             {
-                opt.AllowAnyHeader ().AllowAnyMethod ().WithOrigins ("http://localhost:3000");
+                opt.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
             });
 
             app.UseAuthorization ();
